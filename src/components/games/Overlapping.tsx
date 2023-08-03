7c367b61e0b6ac88a4fb3440cee873fcdf44d8f3
salmonco <xkqjsslsek80@naver.com>
2023-08-03 14:56:41 +0900

    Feat: Add difficulty to overlapping game

--- src/components/games/Overlapping.tsx
+++ src/components/games/Overlapping.tsx
@@ -1,17 +1,33 @@
 import { styled } from 'styled-components';
 
-const Num = styled.span`
+const NumContainer = styled.div`
+  position: relative;
+  width: 30rem;
+  height: 40rem;
+  margin: 0 auto;
+  @media screen and (max-width: 767px) {
+    height: 15rem;
+  }
+`;
+
+const Num = styled.span<{ $top: number; $left: number }>`
   position: absolute;
-  top: 50%;
-  left: 50%;
+  top: ${(props) => props.$top}%;
+  left: ${(props) => props.$left}%;
   transform: translate(-50%, -50%);
   font-size: 30rem;
+  @media screen and (max-width: 767px) {
+    font-size: 15rem;
+  }
 `;
 
 const NumBtn = styled.button`
   padding: 3rem;
   margin: 1rem;
-  font-size: 4rem;
+  font-size: 10rem;
+  @media screen and (max-width: 767px) {
+    font-size: 2rem;
+  }
 `;
 
-export { Num, NumBtn };
+export { NumContainer, Num, NumBtn };
--- src/pages/games/Overlapping.tsx
+++ src/pages/games/Overlapping.tsx
@@ -1,9 +1,21 @@
-import { Num, NumBtn } from '../../components/games/Overlapping';
+import { NumContainer, Num, NumBtn } from '../../components/games/Overlapping';
 
-const answer = [1, 3, 6];
-
+/**
+ * 난도별 겹쳐진 숫자의 개수 상이
+ * 하 : 2
+ * 중 : 3
+ * 상 : 4
+ */
 export default function Overlapping() {
+  let difficulty = 2;
+  let answer: number[] = [];
   let cnt = 0;
+
+  while (answer.length < difficulty + 1) {
+    answer.push(Math.floor(Math.random() * 10));
+    answer = [...new Set(answer)];
+  }
+  console.log(answer);
 
   const checkAnswer = (num: number, el: HTMLButtonElement) => {
     if (!answer.includes(num)) {
@@ -23,15 +35,22 @@ export default function Overlapping() {
   return (
     <>
       <h1>겹쳐진 {answer.length}개의 숫자를 보고 있습니다. 어떤 숫자인가요?</h1>
+      <NumContainer>
+        {answer.map((num, index) => (
+          <Num
+            key={index}
+            $top={50 + Math.floor(Math.random() * 8)}
+            $left={50 + Math.floor(Math.random() * 8)}>
+            {num}
+          </Num>
+        ))}
+      </NumContainer>
       {Array.from({ length: 10 }).map((_, index) => (
         <NumBtn
           key={index}
           onClick={(e) => checkAnswer(index, e.target as HTMLButtonElement)}>
           {index}
         </NumBtn>
-      ))}
-      {answer.map((num, index) => (
-        <Num key={index}>{num}</Num>
       ))}
     </>
   );
