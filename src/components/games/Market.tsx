58bc1e47767e212cc3be59a573fdf0712abb9a42
salmonco <xkqjsslsek80@naver.com>
2023-08-04 18:30:52 +0900

    Feat: generate ramdom answers in market game

--- src/components/games/Market.tsx
+++ src/components/games/Market.tsx
@@ -10,25 +10,39 @@ const Memo = styled.div`
   background: lemonchiffon;
   margin: 0 auto;
   font-size: 4rem;
+  @media screen and (max-width: 767px) {
+    width: 10rem;
+    height: 10rem;
+    font-size: 2rem;
+  }
 `;
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
   font-size: 3rem;
   margin: 1rem 0;
+  flex-wrap: wrap;
 `;
 
 const Img = styled.img`
   width: 30rem;
   height: 30rem;
   margin: 0 2rem 0 0;
+  @media screen and (max-width: 767px) {
+    width: 10rem;
+    height: 10rem;
+  }
 `;
 
 const Button = styled.button`
   padding: 4rem;
   margin: 1rem;
+  font-size: 5rem;
+  @media screen and (max-width: 767px) {
+    font-size: 2rem;
+  }
 `;
 
 export { Memo, Container, Img, Button };
--- src/pages/games/Market.tsx
+++ src/pages/games/Market.tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Memo, Container, Img, Button } from '../../components/games/Market';
 import { useLocation, useNavigate } from 'react-router';
@@ -22,6 +21,22 @@ export default function Market() {
     gameData[gameIndex].problemPool,
   );
   const discountPercent = gameData[gameIndex].discountPercent;
+  let answer = problemPool.reduce((p, c) => p + c.price * c.count, 0);
+  if (discountPercent) {
+    answer = answer * (1 - discountPercent / 100);
+  }
+  const difference = 500;
+  const candidateCnt = 5;
+  let candidate = [answer];
+  while (candidate.length < candidateCnt) {
+    const price =
+      answer +
+      (Math.random() > 0.5 ? 1 : -1) *
+        Math.floor(Math.random() * 5) *
+        difference;
+    if (price > 0 && !candidate.includes(price)) candidate.push(price);
+  }
+  candidate.sort(() => Math.random() - 0.5);
 
   const checkAnswer = (el: HTMLElement) => {
     if (+el.innerText === answer) {
@@ -30,11 +45,6 @@ export default function Market() {
       alert('틀렸습니다 ㅜ.ㅜ');
     }
   };
-
-  let answer = problemPool.reduce((p, c) => p + c.price * c.count, 0);
-  if (discountPercent) {
-    answer = answer * (1 - discountPercent / 100);
-  }
 
   useEffect(() => {
     if (isGameEnded) {
@@ -67,7 +77,11 @@ export default function Market() {
           />
           <h1>
             시장에서 장을 보기 위해, 구매할 품목을 메모지에 적어놓았습니다.
-            <br />총 얼마를 지불해야 할까요?
+            <br />
+            {discountPercent
+              ? `${discountPercent}% 할인 쿠폰을 사용할 때, `
+              : null}
+            총 얼마를 지불해야 할까요?
           </h1>
           <Memo>
             {problemPool.map((v, i) => (
@@ -76,20 +90,22 @@ export default function Market() {
               </span>
             ))}
           </Memo>
-          {problemPool.map((item, index) => (
-            <Container key={index}>
-              <Img src={item.imgUrl} />
-              <div>
-                <p>{item.contents}</p>
-                <p>{item.price}원</p>
+          <Container>
+            {problemPool.map((item, index) => (
+              <div key={index}>
+                <Img src={item.imgUrl} />
+                <div>
+                  <p>{item.contents}</p>
+                  <p>{item.price}원</p>
+                </div>
               </div>
-            </Container>
-          ))}
-          {Array.from({ length: 5 }).map((_, i) => (
+            ))}
+          </Container>
+          {candidate.map((price, i) => (
             <Button
               key={i}
               onClick={(e) => checkAnswer(e.target as HTMLElement)}>
-              {answer + i * 1000}
+              {price}
             </Button>
           ))}
         </>
