75519a78cf149e639c83f01d1c79625909a2788c
salmonco <xkqjsslsek80@naver.com>
2023-08-11 17:33:30 +0900

    Design: just for tap bar toggle test

---
+++ src/pages/Family.tsx
@@ -0,0 +1,13 @@
+import LeftTapBar from '../components/common/LeftTapBar';
+import BottomTapBar from '../components/common/BottomTapBar';
+
+function Family() {
+  return (
+    <>
+      <LeftTapBar />
+      <BottomTapBar />
+    </>
+  );
+}
+
+export default Family;
