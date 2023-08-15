2334ecbd3ef8edbcbf7293975e39e4e8b8f2cd8e
salmonco <xkqjsslsek80@naver.com>
2023-08-15 15:55:19 +0900

    Feat: admin login

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -25,6 +25,12 @@ function LogIn() {
   };
 
   const sendCode = async () => {
+    if (phoneNumber === '01012345678') {
+      alert(
+        '해당 계정은 어드민 계정입니다. 인증번호 입력란에 제공된 비밀번호를 입력하고 로그인해주세요.',
+      );
+      return;
+    }
     const phoneNumberRegex = /^01(0|1|[6-9])[0-9]{3,4}[0-9]{4}$/;
     if (!phoneNumberRegex.test(phoneNumber)) {
       alert('전화번호를 올바르게 입력해주세요.');
@@ -45,52 +51,59 @@ function LogIn() {
     }
   };
 
-  const logIn = async () => {
+  const handleLogIn = () => {
+    const logIn = async () => {
+      try {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API_URL}/patient/login`,
+          {
+            phoneNumber,
+            status: 'ACTIVE',
+            authorities: [
+              {
+                authority: 'PATIENT',
+              },
+            ],
+          },
+        );
+        if (!data.isSuccess) {
+          alert(data.message);
+          return;
+        }
+        const { name, nickname, fontSize, familyKey } =
+          data.result.patientDetailDto;
+        dispatch(
+          userSlice.actions.setUser({
+            name,
+            nickname,
+            fontSize,
+            phoneNumber,
+            familyKey,
+            accessToken: data.result.tokenDto.accessTokenDto.accessToken,
+          }),
+        );
+        await localStorage.setItem(
+          'refreshToken',
+          data.result.tokenDto.refreshTokenDto.refreshToken,
+        );
+        await localStorage.setItem(
+          'accessToken',
+          data.result.tokenDto.accessTokenDto.accessToken,
+        );
+        navigate('/home');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (phoneNumber === '01012345678' && code === '12345678') {
+      logIn();
+      return;
+    }
     if (code !== authNum) {
       alert('인증번호가 올바르지 않습니다. 다시 입력해주세요.');
       return;
     }
-    try {
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_API_URL}/patient/login`,
-        {
-          phoneNumber,
-          status: 'ACTIVE',
-          authorities: [
-            {
-              authority: 'PATIENT',
-            },
-          ],
-        },
-      );
-      if (!data.isSuccess) {
-        alert(data.message);
-        return;
-      }
-      const { name, nickname, fontSize, familyKey } =
-        data.result.patientDetailDto;
-      dispatch(
-        userSlice.actions.setUser({
-          name,
-          nickname,
-          fontSize,
-          phoneNumber,
-          familyKey,
-          accessToken: data.result.tokenDto.accessTokenDto.accessToken,
-        }),
-      );
-      await localStorage.setItem(
-        'refreshToken',
-        data.result.tokenDto.refreshTokenDto.refreshToken,
-      );
-      await localStorage.setItem(
-        'accessToken',
-        data.result.tokenDto.accessTokenDto.accessToken,
-      );
-      navigate('/home');
-    } catch (error) {
-      console.error(error);
-    }
+    logIn();
   };
 
   const toSignUp = () => {
@@ -119,7 +132,7 @@ function LogIn() {
             callbackFn={onChangeCode}
           />
           <Margin3 />
-          <Button disabled={!phoneNumber || !code} onClick={logIn}>
+          <Button disabled={!phoneNumber || !code} onClick={handleLogIn}>
             로그인
           </Button>
         </Wrapper>
