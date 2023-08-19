c5221fdb404eb55581136eaac03974d9e772e6c6
salmonco <xkqjsslsek80@naver.com>
2023-08-19 16:48:45 +0900

    Design: privacy

--- src/pages/PhoneNumberSet.tsx
+++ src/pages/PhoneNumberSet.tsx
@@ -13,6 +13,7 @@ function PhoneNumberSet() {
   const [code, setCode] = useState('');
   const [authNum, setAuthNum] = useState('');
   const navigate = useNavigate();
+  const [isCheckedPrivacy, setIsCheckedPrivacy] = useState(false);
 
   const onChangePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(e.target.value.trim());
@@ -75,8 +76,31 @@ function PhoneNumberSet() {
             value={code}
             callbackFn={onChangeCode}
           />
+          <CheckContainer>
+            <CheckboxLabel htmlFor="privacy">
+              <CheckboxInput
+                id="privacy"
+                type="checkbox"
+                checked={isCheckedPrivacy}
+                onChange={() => setIsCheckedPrivacy((prev) => !prev)}
+              />
+              개인정보처리방침 및 이용약관에 동의합니다.
+            </CheckboxLabel>
+            <PrivacyButton
+              onClick={() =>
+                window.open(
+                  'http://brain-vitamin-web-front-deploy.s3-website.ap-northeast-2.amazonaws.com/privacy',
+                  '_blank',
+                )
+              }>
+              <span>* 개인정보처리방침 보기</span>
+              <span style={{ color: 'gray', fontWeight: 'bold' }}>{'>'}</span>
+            </PrivacyButton>
+          </CheckContainer>
         </Wrapper>
-        <Button disabled={!phoneNumber || !code} onClick={goNext}>
+        <Button
+          disabled={!phoneNumber || !code || !isCheckedPrivacy}
+          onClick={goNext}>
           다음
         </Button>
       </Box>
@@ -125,6 +149,52 @@ const Margin2 = styled.div`
   @media screen and (max-width: 767px) {
     margin: 0 0 0.8rem 0;
   }
+`;
+const CheckContainer = styled.div`
+  font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  width: 41.2rem;
+  margin: 0 auto;
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    font-size: 1.6rem;
+  }
+`;
+const CheckboxLabel = styled.label`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  margin: 2rem 0;
+  @media screen and (max-width: 767px) {
+    gap: 0.5rem;
+  }
+`;
+const CheckboxInput = styled.input`
+  width: 2rem;
+  height: 2rem;
+  border: 0.1rem solid #1f1411;
+  position: relative;
+  &:checked {
+    &::before {
+      content: '\\2713';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      font-size: 3.5rem;
+      color: var(--main-color);
+    }
+  }
+`;
+const PrivacyButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  background: var(--main-bg-color);
+  padding: 1rem;
+  border-radius: 1rem;
+  font-size: 2rem;
+  align-items: center;
 `;
 
 export default PhoneNumberSet;
