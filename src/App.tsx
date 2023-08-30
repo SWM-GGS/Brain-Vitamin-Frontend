084959c731c813a85ddacb457c0016924a103d6f
salmonco <xkqjsslsek80@naver.com>
2023-08-30 16:27:56 +0900

    Feat: connect ga4 #26

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Provider } from 'react-redux';
 import store from './store/index.ts';
 import AppInner from './AppInner.tsx';
+import RouteChangeTracker from './modules/RouteChangeTracker.tsx';
 
 function App() {
+  RouteChangeTracker();
+
   return (
     <Provider store={store}>
       <AppInner />
