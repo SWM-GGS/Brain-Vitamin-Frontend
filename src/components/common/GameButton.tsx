2a39f14c457f6c220d120bfb28db8edea05eab00
salmonco <xkqjsslsek80@naver.com>
2023-09-17 18:34:20 +0900

    Design: 게임 버튼 공통 컴포넌트에 medium size 추가

--- src/components/common/GameButton.tsx
+++ src/components/common/GameButton.tsx
@@ -12,7 +12,7 @@ const ButtonContainer = styled.div`
     gap: 2rem;
   }
 `;
-const Button = styled.button<{ $isLong?: boolean }>`
+const Button = styled.button<{ $isLong?: boolean; $isMedium?: boolean }>`
   padding: 3rem 4rem;
   font-size: 6rem;
   border-radius: 1rem;
@@ -21,17 +21,20 @@ const Button = styled.button<{ $isLong?: boolean }>`
   border: 0.2rem solid var(--gray-bg-color);
   ${(props) =>
     props.$isLong ? `min-width: 365px;` : `width: 130px; height: 130px;`}
+  ${(props) => props.$isMedium && `width: 220px; height: 220px;`}
   @media screen and (min-width: 768px) and (max-height: 1079px) {
     font-size: 3rem;
     padding: 1.5rem 2rem;
     ${(props) =>
       props.$isLong ? `min-width: 230px;` : `width: 70px; height: 70px;`}
+    ${(props) => props.$isMedium && `width: 130px; height: 130px;`}
   }
   @media screen and (max-width: 767px) {
     font-size: 2rem;
     padding: 1.5rem 2rem;
     ${(props) =>
       props.$isLong ? `min-width: 230px;` : `width: 60px; height: 60px;`}
+    ${(props) => props.$isMedium && `width: 100px; height: 100px;`}
   }
 `;
 
